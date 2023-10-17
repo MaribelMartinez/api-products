2d944ff51fa6c97c3090fbe2663d0123c3d4cee2
Maribel <martinezmaribel1994@gmail.com>
2023-10-16 21:14:31 -0300

    add validators

--- pkg/api/app/domain/product.go
+++ pkg/api/app/domain/product.go
@@ -1,1 +1,11 @@
 package domain
+
+type Product struct {
+	Sku            string   `json:"sku"`
+	Name           string   `json:"name" validate:"required,max=50,min=3"`
+	Brand          string   `json:"brand" validate:"required,max=50,min=3"`
+	Size           string   `json:"size"`
+	Price          float64  `json:"price" validate:"required"`
+	PrincipalImage string   `json:"principal_image" validate:"required"`
+	OtherImages    []string `json:"other_images"`
+}
--- pkg/api/helpers/validator.go
+++ pkg/api/helpers/validator.go
@@ -1,2 +1,57 @@
 package helpers
 
+import (
+	"api-products-maribel-martinez/pkg/api/app/domain"
+	"api-products-maribel-martinez/pkg/api/app/errors"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	url1 "net/url"
+	"strings"
+)
+
+func validateProduct(product domain.Product) error {
+	validate := validator.New()
+	if err := validate.Struct(product); err != nil {
+		return err
+	}
+	if err := validateBlankValuesProduct(product); err != nil {
+		return err
+	}
+	if err := validateImagesUrls(product); err != nil {
+		return err
+	}
+}
+
+func validateBlank(value string) bool {
+	return len(strings.TrimSpace(value)) == len(value)
+}
+
+func validateUrlFormat(url string) bool {
+	u, err := url1.Parse(url)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+func validateBlankValuesProduct(product domain.Product) error {
+	if validateBlank(product.Name) {
+		return errors.ErrorFromMessage{Message: "Name can't be blank"}
+	}
+	if validateBlank(product.Brand) {
+		return errors.ErrorFromMessage{Message: "Brand can't be blank"}
+	}
+	if validateBlank(product.Size) {
+		return errors.ErrorFromMessage{Message: "Size can't be blank"}
+
+	}
+	return nil
+}
+
+func validateImagesUrls(product domain.Product) error {
+	if !validateUrlFormat(product.PrincipalImage) {
+		return errors.ErrorFromMessage{Message: "the url of the principal image is not valid"}
+	}
+	for i, image := range product.OtherImages {
+		if !validateUrlFormat(image) {
+			return errors.ErrorFromMessage{Message: fmt.Sprintf("the url of other image in position %d is not valid", i)}
+		}
+	}
+}
